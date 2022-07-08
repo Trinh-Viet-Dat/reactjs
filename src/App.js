6b1221410bb07b19a047f5b2a2012d9112a3d5f1
TrinhDat <cv.trinhvietdat1805@gmail.com>
2022-07-08 22:21:06 +0700

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React from "react"
 
-function App() {
+const App = () => {
+  const [page, setPage] = React.useState(1)
+  const [data, setData] = React.useState()
+  React.useEffect(() => {
+    axios.get(`https://randomuser.me/api/?page=${page}&results=10`).then(res => {
+      console.log(res.data.results)
+      setData(res.data.results)
+    })
+  }, [page])
+  const handlePageAugment = () => {
+    setPage(page + 1)
+  }
+  const handlePageDecrease = () => {
+    setPage(page - 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '5%' }}>
+      <table className="table" style={{ textAlign: 'center' }}>
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Full Name</th>
+            <th scope="col">UserName</th>
+            <th scope="col">Thumbnail Icon</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            (data ? data.map((e, index) => {
+              return (
+                <tr key={index}>
+                  <th scope="row">{(page - 1) * 10 + index + 1}</th>
+                  <td>{e.name.title + e.name.first + e.name.last}</td>
+                  <td>{e.login.username}</td>
+                  <td><img src={e.picture.large} style={{ width: '30px', height: '30px' }} /></td>
+                </tr>
+              )
+            }) : <tr>No data</tr>)
+          }
+        </tbody>
+      </table>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination justify-content-center">
+          <li className="page-item ">
+            <button style={{ margin: '10px', borderRadius: '5px', width: '90px' }} disabled={page === 1} onClick={handlePageDecrease} >Previous</button>
+          </li>
+          <li className="page-item">
+            <button style={{ margin: '10px', borderRadius: '5px', width: '90px' }} disabled={page === 10} onClick={handlePageAugment} >Next</button>
+          </li>
+        </ul>
+      </nav>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
